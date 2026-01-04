56a898a5597b985dc44f5314c7894ce3bba096b4
angelo-opustech <angelo@opustech.com.br>
2026-01-04 00:20:49 -0300

    Corrigir caminhos em main.sh: usar absolutos para compatibilidade com link simbólico

--- main.sh
+++ main.sh
@@ -4,25 +4,25 @@
 # Inclui todos os módulos necessários
 
 # Incluir constantes
-source "$(dirname "$0")/constants.sh"
+source "/usr/sap/SmartSafeHanaOpusCloud/constants.sh"
 
 # Incluir funções
-source "$(dirname "$0")/functions.sh"
+source "/usr/sap/SmartSafeHanaOpusCloud/functions.sh"
 
 # Exibir banner
-source "$(dirname "$0")/banner.sh"
+source "/usr/sap/SmartSafeHanaOpusCloud/banner.sh"
 
 # Lógica de configuração principal
-source "$(dirname "$0")/config_logic.sh"
+source "/usr/sap/SmartSafeHanaOpusCloud/config_logic.sh"
 
 # Configuração de caminhos
-source "$(dirname "$0")/path_config.sh"
+source "/usr/sap/SmartSafeHanaOpusCloud/path_config.sh"
 
 # Configuração de cópia
-source "$(dirname "$0")/copy_config.sh"
+source "/usr/sap/SmartSafeHanaOpusCloud/copy_config.sh"
 
 # Configuração de email
-source "$(dirname "$0")/email_config.sh"
+source "/usr/sap/SmartSafeHanaOpusCloud/email_config.sh"
 
 # Geração do script agendado e agendamento
-source "$(dirname "$0")/scheduled_gen.sh"
+source "/usr/sap/SmartSafeHanaOpusCloud/scheduled_gen.sh"
